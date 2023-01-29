224187a203bfd86d7185b3eab2830434a2ec749d
unknown <imbesatanwar90@gmail.com>
2023-01-29 19:44:18 +0530

    jenkins files paramete3

--- parameter_add.py
+++ parameter_add.py
@@ -1,8 +1,7 @@
+ import os
 def add(a, b):
 	print("Gong to add two number")
 	print("Addition", a+b)
-    
- import os
 input1 = os.getenv("Input1")
 input2 = os.getenv("input2")
 add(input1,input2)
